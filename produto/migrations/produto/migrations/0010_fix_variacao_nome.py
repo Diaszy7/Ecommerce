12aafe26b88e543e85ab89e7f7f8d27fd34e42b1
Gabriel Dias <128110247+Diaszy7@users.noreply.github.com>
2026-01-09 15:52:28 -0300

    Add empty migration to fix variacao nome
    Create an empty migration to align Django state with the database.

---
+++ produto/migrations/produto/migrations/0010_fix_variacao_nome.py
@@ -0,0 +1,12 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('produto', '0009_auto_20190815_1147'),
+    ]
+
+    operations = [
+        # migration vazia apenas para alinhar estado do Django com o banco
+    ]
